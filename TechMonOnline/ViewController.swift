0db316cad85f7739d60b3a8e6175ffe4fbe444c1
Yo Higashida <penguin4731@gmail.com>
2021-02-15 00:49:35 +0900

    Initial Commit

---
+++ TechMonOnline/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechMonOnline
+//
+//  Created by Yo Higashida on 2021/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
